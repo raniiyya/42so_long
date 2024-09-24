e01acc069aebeb5e4debdf4bd1b3f7da0b219523
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-09-24 15:16:39 +0000

    header

---
+++ so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 11:30:02 by rdavurov          #+#    #+#             */
+/*   Updated: 2024/09/24 15:07:12 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <fcntl.h>
+# include "get_next_line/get_next_line.h"
+
+void parse_map(char **map, char *file);
+
+#endif
