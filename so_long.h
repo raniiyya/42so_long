94ec62f98676313737543c8820812c041a35fbd4
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-10-03 10:05:53 +0000

    new

--- so_long.h
+++ so_long.h
@@ -6,24 +6,17 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:30:02 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/09/29 18:36:04 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/03 09:59:43 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <fcntl.h>
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
-
-typedef struct s_game
-{
-    char    **map;
-    void    *mlx;
-    void    *window;
-}             t_game;
+# include <fcntl.h>
 
 typedef struct s_txt
 {
@@ -36,14 +29,22 @@ typedef struct s_txt
 	void	*pr;
 	void	*m;
 	void	*e;
-}	t_txt;
+}			t_txt;
 
-char **parse_map(char **map, char *file);
-void validate_map(char **map);
-void validate_chars(char **map);
-void validate_rect(char **map);
-void validate_path(char **map);
-void validate_border(char **map);
-void    init_game(t_game *game);
+typedef struct s_game
+{
+	char	**map;
+	void	*mlx;
+	void	*window;
+	t_txt	textures;
+}			t_game;
+
+char		**parse_map(char **map, char *file);
+void		validate_map(char **map);
+void		validate_chars(char **map);
+void		validate_rect(char **map);
+void		validate_path(char **map);
+void		validate_border(char **map);
+void		init_game(t_game *game);
 
 #endif
--- visual/init_game.c
+++ visual/init_game.c
@@ -6,16 +6,41 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:45:31 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/09/29 18:37:09 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/03 10:00:29 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void    init_game(t_game *game)
+void	fill_textures(t_game *game)
 {
-    game->mlx = mlx_init();
-    game->window = mlx_new_window(game->mlx, 900, 600, "so_long");
-    mlx_loop(game->mlx);
-    
+	int	height;
+
+	height = 50;
+	game->textures.b = mlx_xpm_file_to_image(game->mlx,
+			"../textures/background.xpm", &height, &height);
+	game->textures.c = mlx_xpm_file_to_image(game->mlx, "../textures/money.xpm",
+			&height, &height);
+	game->textures.e = mlx_xpm_file_to_image(game->mlx, "../textures/house.xpm",
+			&height, &height);
+	game->textures.w = mlx_xpm_file_to_image(game->mlx, "../textures/wall.xpm",
+			&height, &height);
+    game->textures.pd = mlx_xpm_file_to_image(game->mlx, "../textures/krabs.xpm",
+            &height, &height);
+    game->textures.pl = mlx_xpm_file_to_image(game->mlx, "../textures/pleft.xpm",
+            &height, &height);
+    game->textures.pr = mlx_xpm_file_to_image(game->mlx, "../textures/pright.xpm",
+            &height, &height);
+    game->textures.pu = mlx_xpm_file_to_image(game->mlx, "../textures/pback.xpm",
+            &height, &height);
+    game->textures.m = mlx_xpm_file_to_image(game->mlx, "../textures/monster.xpm",
+            &height, &height);
+}
+
+void	init_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->window = mlx_new_window(game->mlx, 900, 600, "so_long");
+	fill_textures(game);
+	mlx_loop(game->mlx);
 }
