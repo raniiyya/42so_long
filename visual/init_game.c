5f7ece1b352e38806590877c82ec221e6e2570bb
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-10-06 18:32:55 +0000

    updates + comments

--- visual/init_game.c
+++ visual/init_game.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:45:31 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/10/03 10:00:29 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/06 18:32:39 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,48 @@ void	fill_textures(t_game *game)
             &height, &height);
 }
 
-void	init_game(t_game *game)
+void	render_map(t_game *game)
 {
-	game->mlx = mlx_init();
-	game->window = mlx_new_window(game->mlx, 900, 600, "so_long");
-	fill_textures(game);
-	mlx_loop(game->mlx);
+	int i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	printf("%c", game->map[i][j]);
+	while (game->map[i])
+	{
+		while (game->map[i][j])
+		{
+			printf("%c", game->map[i][j]);
+			j++;
+		}
+			i++;
+	}
+}
+
+int	close_game(t_game *game)
+{
+	(void)game;
+	exit(0);
+	return (42);
+}
+int	handle_key_events(int keycode, t_game *game)
+{
+	if (keycode == 65307)
+		close_game(game);
+	return (42);
+}
+
+void init_game(t_game *game)
+{
+    game->mlx = mlx_init();
+    game->window = mlx_new_window(game->mlx, 900, 600, "so_long");
+
+
+    fill_textures(game); // Ensure this is defined and working correctly
+    render_map(game);    // Ensure this renders your map correctly
+
+    mlx_hook(game->window, 17, 0, close_game, game);           // Handle window close event
+    mlx_key_hook(game->window, handle_key_events, game);       // Handle key press events
+    mlx_loop(game->mlx);                                       // Start the MLX loop
 }
