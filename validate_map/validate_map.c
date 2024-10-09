e0d9708393130ba99e9503b6aa7180275082cb9b
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-10-09 19:08:02 +0000

    updated

--- validate_map/validate_map.c
+++ validate_map/validate_map.c
@@ -6,16 +6,16 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:43:46 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/10/09 19:06:16 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/09 19:07:56 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void validate_map(char **map)
+void validate_map(t_game *game)
 {
     validate_chars(game->map);
     validate_rect(game->map);
     validate_border(game->map);
-    validate_path(map);
+    validate_path(game);
 }
