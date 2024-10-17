3ef0cf0c963fe2eebfe3ce73ea2a7882f0024639
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-10-17 17:07:41 +0000

    enhanced version

--- validate_map/validate_border.c
+++ validate_map/validate_border.c
@@ -6,47 +6,35 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:43:11 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/09/29 18:34:53 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/17 17:07:28 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void validate_border(char **map)
+void	validate_border(char **map)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
+	size_t	width;
+	size_t	height;
 
-    j = 0;
-    while (map[0][j])
-    {
-        if (map[0][j] != '1') //checking the 1st line
-        {
-            write(1, "Error\nInvalid map\n", 18);
-            exit(1);
-        }
-        j++;
-    }
-    i = 0;
-    j = 0;
-    while (map[i]) //going to the last line
-        i++;
-    while (map[i - 1][j])
-    {
-        if (map[i - 1][j] != '1') //cheching the last line
-        {
-            write(1, "Error\nInvalid map\n", 18);
-            exit(1);
-        }
-        j++;
-    }
-    i = 0;
-    j = 0;
-    while (map[i][j]) //going to the last column i guess
-        j++;
-    if (map[i][0] != '1' && map[i][j] != '1') //checking the 1st and last column
-    {
-        write(1, "Error\nInvalid map\n", 18);
-        exit(1);
-    }
+	width = map_width(map) - 1;
+	height = map_height(map) - 1;
+	i = -1;
+	while (++i, map[i])
+	{
+		j = -1;
+		while (++j, map[i][j])
+		{
+			if (i == 0 || i == height || j == 0 || j == width)
+			{
+				if (map[i][j] != '1')
+				{
+					write(1, "Invalid map!\n", 13);
+					exit(1);
+				}
+			}
+		}
+	}
 }
